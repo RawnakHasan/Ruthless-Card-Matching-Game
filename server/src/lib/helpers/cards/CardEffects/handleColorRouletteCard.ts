4638674726b3cf450a71f7344a5da6cb24aea9b7
Rawnak Hasan <rawnakhasan86@gmail.com>
2026-01-14 13:44:22 +0600

    Color Roulette Effects Done

--- server/src/lib/helpers/cards/CardEffects/handleColorRouletteCard.ts
+++ server/src/lib/helpers/cards/CardEffects/handleColorRouletteCard.ts
@@ -4,31 +4,24 @@ import { drawCards } from "../../game/drawCards";
 
 export const handleColorRouletteCard = (game: Game, card: WildCard) => {
   const chosenColor = card.chosenColor;
-
-  if (!chosenColor) {
-    console.error("Color Roulette card played without chosen color");
-    return;
-  }
+  if (!chosenColor) return;
 
   const targetPlayer = getNextPlayer(game);
 
-  if (!targetPlayer) {
-    console.log("Target player not found");
-    return;
-  }
-
-  // Draw until a card matches the chosen color and is normal/action (not Wild)
   while (true) {
     if (game.deck.length === 0) reshuffle(game);
 
     const [drawnCard] = drawCards(game, 1);
-
     if (!drawnCard) break;
 
     targetPlayer.hand.push(drawnCard);
 
     if (drawnCard.type !== "Wild" && drawnCard.color === chosenColor) {
-      break; // stop drawing
+      break;
     }
   }
+
+  game.drawCount = 0;
+
+  game.playerTurn = targetPlayer.id;
 };
